9d3fbcd919f0bb456da4ba09e9675bcf0fde086e
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2022-05-08 16:22:44 -0700

    used sudo -u flags so back to one script

--- install.sh
+++ install.sh
@@ -19,4 +19,30 @@ git clone https://github.com/Nickiel12/awesome-dot-files
 chmod +x /home/$USER/awesome-dot-files/user-install.sh
 chmod +x /home/$USER/awesome-dot-files/yay_installs.sh
 
-echo "Now switch to $USER and run user-install.sh"
+pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay
+sudo -u nicholas makepkg -si
+
+sudo -u nicholas$cur_dir"/yay_installs.sh"
+
+chmod +x $cur_dir"/src/home/.config/ranger/scope.sh"
+chmod +x $cur_dir"/src/home/.config/autostart.sh"
+chmod +x $cur_dir"/src/home/.config/.sh"
+
+# Recreate and copy config files
+# the \cp overwrite the alias to cp=cp -i, so there will be no confirmation
+# the -T flag does not create a source dir in the target folder
+/bin/cp -RT $cur_dir"/src/etc" "/"
+/bin/cp -RT $cur_dir"/src/home" "/home/"$USER
+
+# https://tecadmin.net/adding-line-in-middle-of-file-using-command-in-linux/
+# -i "##i..." the -i says insert, and the ## before i says what line
+sed -i "19izstyle :compinstall filename '/home/$USER/.zshrc'" /home/$USER/.zshrc
+
+usermod -s /usr/bin/zsh $USER
+
+systemctl enable NetworkManager
+# github.com/jceb/dex
+# Use for autostarting .desktop files
+systemctl --user add-wants autostart.target polkit-dumb-agent.service
+# lightdm will start xorg, and xinit will start awesome
+systemctl enable lightdm
