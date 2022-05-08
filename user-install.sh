6e178794821a4b40b852c5b406359489924e3d26
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2022-05-08 09:11:44 -0700

    bug fixes

--- user-install.sh
+++ user-install.sh
@@ -1,16 +1,19 @@
+
+cur_dir=`dirname $(realpath $0)`
+
 #from yay src github
 pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 $cur_dir"/yay_installs.sh"
 
-# https://tecadmin.net/adding-line-in-middle-of-file-using-command-in-linux/
-# -i "##i..." the -i says insert, and the ## before i says what line
-sed -i "19izstyle :compinstall filename '/home/$USER/.zshrc'" /home/$USER/.zshrc
-
 # Recreate and copy config files
 # the \cp overwrite the alias to cp=cp -i, so there will be no confirmation
 sudo /bin/cp -R $cur_dir"/src/etc" "/"
 sudo /bin/cp -R $cur_dir"/src/home" "/home/"$USER
+
+# https://tecadmin.net/adding-line-in-middle-of-file-using-command-in-linux/
+# -i "##i..." the -i says insert, and the ## before i says what line
+sed -i "19izstyle :compinstall filename '/home/$USER/.zshrc'" /home/$USER/.zshrc
 
 sudo systemctl enable NetworkManager
 # github.com/jceb/dex
