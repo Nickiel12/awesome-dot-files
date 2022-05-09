6cc880cb81a2da109d0405be45892d126e168052
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2022-05-08 21:58:02 -0700

    added systemd settings

--- update.sh
+++ update.sh
@@ -12,6 +12,7 @@ while [ $# -gt 0 ] ; do
     -a | --awesome) sudo $cur_dir/scripts/update_awesome_configs.sh $HOME;;
     -s | --shell)   sudo $cur_dir/scripts/update_shell_configs.sh $HOME;;
     -r | --ranger)  sudo $cur_dir/scripts/update_ranger_configs.sh $HOME ;;
+    --sys)          sudo $cur_dir/scripts/update_systemd_files.sh $HOME ;;
     --all)          sudo $cur_dir/scripts/update_all.sh $HOME ;;
 
     -h | --help) 
@@ -21,6 +22,7 @@ several flags can be passed, but for now, they must be individual flags (e.g. -a
 -a, --awesome : awesome wm configs
 -s, --shell   : zsh, powerline, and .Xresources
 -r, --ranger  : ranger configs
+--sys         : systemd user settings
 --all         : updates all config files, even ones that don't have a dedicted flag above
 "
   esac
