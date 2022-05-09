5629a1d60bf28d1616872cf4e44e7ad2965cce80
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2022-05-08 21:58:20 -0700

    fixed accidental copy

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -5,7 +5,7 @@ echo "WARNING: This WILL override ALL existing settings. Are you sure? (y/n)"
 read response
 
 if ($respone -eq "y") then
-    /bin/cp -RT $cur_dir/../src/home/.config/ranger $home_dir/.config/ranger
+    /bin/cp -RT $cur_dir/../src/home/.config/ $home_dir/.config/
     /bin/cp -RT $cur_dir/../src/etc /etc
 else 
     echo "Aborting"
