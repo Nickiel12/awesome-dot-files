89d635cc099be8dc82c91cc2dcbb8613beb6ccd7
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2022-05-11 09:23:41 -0700

    added dejavu fonts for powerline

--- yay-installs.sh
+++ yay-installs.sh
@@ -3,4 +3,5 @@ yay -S --noconfirm i3lock-fancy-git
 yay -S --noconfirm lightdm-settings
 yay -S --noconfirm visual-studio-code-bin
 yay -S --noconfirm ttf-meslo-nerd-font-powerlevel10k
+yay -S --noconfirm ttf-dejavu-sans-mono-powerline-git
 yay -S --noconfirm polkit-dumb-agent-git
