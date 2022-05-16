2a6bcd860b1d8ff8bc9fa6ecab5de987ce4fddfe
Nickiel12 <nicholasyoungsumner@gmail.com>
2022-05-16 13:38:46 -0700

    fixed dbus header error

--- user-install.sh
+++ user-install.sh
@@ -8,7 +8,7 @@ cd ..
 cd ./lua-ldbus
 makepkg -si
 
-luarocks install --server=https://luarocks.org/dev lua-dbus
+sudo luarocks install --server=http://luarocks.org/manifests/daurnimator ldbus DBUS_INCDIR=/usr/include/dbus-1.0/ DBUS_ARCH_INCDIR=/usr/lib/dbus-1.0/include
 
 cd ./..
 ./yay-installs.sh
