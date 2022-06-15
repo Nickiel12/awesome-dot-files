16cf18771e5478dc350df5da88d9378a58011365
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2022-06-14 21:12:15 -0700

    trying to renable workspaces

--- src/home/.config/awesome/menu_conf.lua
+++ src/home/.config/awesome/menu_conf.lua
@@ -58,7 +58,23 @@ menu_conf.init = function(
     awful.screen.connect_for_each_screen(function(s)
         -- Wallpaper
         set_wallpaper(s)
-        
+    
+        -- Each screen has its own tag table.
+        awful.tag({ "1", "2", "3", "4", "5" }, s, awful.layout.layouts[1])
+    
+        -- Create a promptbox for each screen
+        s.mypromptbox = awful.widget.prompt()
+        -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+        -- We need one layoutbox per screen.
+        s.mylayoutbox = awful.widget.layoutbox(s)
+        s.mylayoutbox:buttons(gears.table.join(
+                                awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                                awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                                awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                                awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+        -- Create a taglist widget
+
+    
     end)
     -- }}}
 end 
