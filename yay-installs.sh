4ef069b0d349f74248fe1bffed5e36efba43d91f
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2022-06-18 21:40:52 -0700

    added github desktop to default install

--- yay-installs.sh
+++ yay-installs.sh
@@ -5,3 +5,4 @@ yay -S --noconfirm visual-studio-code-bin
 yay -S --noconfirm ttf-meslo-nerd-font-powerlevel10k
 yay -S --noconfirm ttf-dejavu-sans-mono-powerline-git
 yay -S --noconfirm polkit-dumb-agent-git
+yay -S --noconfirm github-desktop-bin
